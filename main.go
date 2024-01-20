7a67badcc08ff56d868df8c59c45dfe5974a9b90
cd-x <whowantsthisemailid@gmail.com>
2024-01-20 16:52:55 +0530

    fixed server is up

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/boltdb/bolt"
 )
 
 var (
-	db_location = flag.String("db_location", "", "Database location")
+	db_location   = flag.String("db_location", "my.db", "Database location")
+	http_location = flag.String("http_address", "localhost:8080", "Address to connect to database")
 )
 
 func parseFlags() {
@@ -22,9 +25,22 @@ func main() {
 
 	parseFlags()
 
-	db, err := bolt.Open("my.db", 0600, nil)
+	db, err := bolt.Open(*db_location, 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	getHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Get database\n")
+	}
+
+	setHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Set database\n")
+	}
+
+	http.HandleFunc("/get", getHandler)
+	http.HandleFunc("/set", setHandler)
+
+	log.Fatal(http.ListenAndServe(*http_location, nil))
 }
